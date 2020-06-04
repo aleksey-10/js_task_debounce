9c61781f44c9a1528ab7657b08c6960072be6a14
aleksey-10 <alexcare1337@gmail.com>
2020-06-04 13:47:44 +0300

    solution

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const input = document.getElementById('input');
+const output = document.querySelector('.output');
+
+function debounce(f, delay) {
+  let timer = setTimeout(() => console.log('waiting for input'), delay);
+
+  return function(event) {
+    clearTimeout(timer);
+    timer = setTimeout(() => f.call(this, event), delay);
+  }
+}
+
+function onChange(event) {
+  output.innerHTML = event.target.value;
+  console.log(this.value); // uncomment if you implemented debounce with correct this
+}
+
+let wrapper = debounce(onChange, 1000);
+
+input.addEventListener('input', wrapper);
